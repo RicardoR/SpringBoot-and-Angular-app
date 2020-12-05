40ddb94bf2b388d9f082f596d730ac78e6212fe2
Ricardo <rirova81@gmail.com>
2020-12-05 13:50:07 +0100

    Add dependencies, fix server.js

--- bike-ui/server.js
+++ bike-ui/server.js
@@ -1,35 +1,35 @@
 // Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const proxy = require('express-http-proxy');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const proxy = require("express-http-proxy");
+const cors = require("cors");
 
 const app = express();
 
 // Parsers for POST data
-app.use(bodyParser.json({limit: '20mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "20mb" }));
 
 app.use(cors());
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist/bike-ui/")));
 
 // Set our api routes proxy to point to spring boot server
-app.use('/server', proxy('http://localhost:8080'));
+app.use("/server", proxy("http://localhost:8080"));
 
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/bike-ui/index.html"));
 });
 
 /**
  * Get port from environment and store in Express.
  */
- const port = '4200';
-app.set('port', port);
+const port = "4200";
+app.set("port", port);
 
 /**
  * Create HTTP server.
